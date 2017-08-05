65106b4fa0c6e48e6c6e59b21dd801dd6cd68592
Nikola Kovacs <nikola.kovacs@gmail.com>
2017-08-05 19:39:56 +0200

    Add debounce (#21)
    Fixes unresponsive cursor movement caused by this package.
    
    Fixes #17

--- spec/go-signature-statusbar-spec.js
+++ spec/go-signature-statusbar-spec.js
@@ -18,6 +18,10 @@ describe('GoSignatureStatusbar', () => {
       atom.packages.triggerActivationHook('core:loaded-shell-environment')
       workspaceElement = atom.views.getView(atom.workspace)
       jasmine.attachToDOM(workspaceElement)
+      // make _.debounce work in atom < 1.9
+      if (!window.Date.now.isSpy) {
+        spyOn(window.Date, 'now').andCallFake(() => window.now)
+      }
     })
 
     waitsForPromise(() => {
@@ -85,48 +89,68 @@ describe('GoSignatureStatusbar', () => {
     it('displays the signature of a function in the status bar', () => {
       runs(() => {
         editor.setCursorScreenPosition([9, 9])
+        advanceClock(301)
       })
 
       waitsFor(() =>
         // First call issued on activating the package, second call issued when
         // opening the file, third call issued when navigating to the position
-        goSignatureStatusbarView.textContent.calls.length === 3
+        goSignatureStatusbarView.textContent.calls.length === 2
       )
       runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Println(a ...interface{}) (n int, err error)'))
     })
 
     it('displays the signature of a function wrapped by another function in the status bar', () => {
-      runs(() => editor.setCursorScreenPosition([9, 23]))
-      waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+      runs(() => {
+        editor.setCursorScreenPosition([9, 23])
+        advanceClock(301)
+      })
+
+      waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
       runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Sprintf(format string, a ...interface{}) string'))
     })
 
     describe('and the cursor is placed behind the closing bracket of a function', () =>
       it('displays the signature in the status bar', () => {
-        runs(() => editor.setCursorScreenPosition([9, 34]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([9, 34])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Sprintf(format string, a ...interface{}) string'))
-        runs(() => editor.setCursorScreenPosition([9, 35]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 4)
+        runs(() => {
+          editor.setCursorScreenPosition([9, 35])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Println(a ...interface{}) (n int, err error)'))
       })
     )
 
     describe('and the cursor is placed on a function that contains line-breaks', () =>
       it('displays the signature in the status bar', () => {
-        runs(() => editor.setCursorScreenPosition([11, 12]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([11, 12])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Print(a ...interface{}) (n int, err error)'))
       })
     )
 
     describe('and the cursor is moved from above a call to a function to somewhere else', () =>
       it('clears the signature in the status bar', () => {
-        runs(() => editor.setCursorScreenPosition([9, 10]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([9, 10])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Println(a ...interface{}) (n int, err error)'))
-        runs(() => editor.setCursorScreenPosition([4, 7]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 4)
+        runs(() => {
+          editor.setCursorScreenPosition([4, 7])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe(''))
       })
     )
@@ -136,16 +160,20 @@ describe('GoSignatureStatusbar', () => {
         runs(() => {
           atom.config.set('go-signature-statusbar.format', '%R %F%A')
           editor.setCursorScreenPosition([9, 10])
+          advanceClock(301)
         })
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('(n int, err error) Println(a ...interface{})'))
       })
     )
 
     describe('and the function has no return value', () =>
       it('formats the output', () => {
-        runs(() => editor.setCursorScreenPosition([15, 9]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([15, 9])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Print(v ...interface{})'))
       })
     )
@@ -165,8 +193,11 @@ describe('GoSignatureStatusbar', () => {
 
     describe('when the function spans multiple lines and "(" is not the last character of the first line', () =>
       it('displays the signature of a function in the status bar', () => {
-        runs(() => editor.setCursorScreenPosition([11, 9]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([11, 9])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Println(a ...interface{}) (n int, err error)'))
       })
     )
@@ -187,26 +218,38 @@ describe('GoSignatureStatusbar', () => {
 
     describe('when function calls are chained together on the same line', () => {
       it('displays the signature of the first function in the chain', () => {
-        runs(() => editor.setCursorScreenPosition([12, 12]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([12, 12])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('NewDecoder(r io.Reader) *json.Decoder'))
       })
 
       it('displays the signature of a function passed as an argument to the first function in the chain', () => {
-        runs(() => editor.setCursorScreenPosition([12, 30]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([12, 30])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('NewReader(s string) *strings.Reader'))
       })
 
       it('displays the signature of the second function in the chain', () => {
-        runs(() => editor.setCursorScreenPosition([12, 63]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([12, 63])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Decode(v interface{}) error'))
       })
 
       it('ignores the "." of a chained call', () => {
-        runs(() => editor.setCursorScreenPosition([12, 57]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([12, 57])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('NewDecoder(r io.Reader) *json.Decoder'))
       })
     })
@@ -227,8 +270,11 @@ describe('GoSignatureStatusbar', () => {
 
     describe('when the function does not return anything', () =>
       it('does not add extra closing bracket to the end of the function', () => {
-        runs(() => editor.setCursorScreenPosition([5, 9]))
-        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 3)
+        runs(() => {
+          editor.setCursorScreenPosition([5, 9])
+          advanceClock(301)
+        })
+        waitsFor(() => goSignatureStatusbarView.textContent.calls.length === 2)
         runs(() => {
           expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).not.toBe('Print(v ...interface{}))')
           expect(workspaceElement.querySelector('div.go-signature-statusbar').textContent).toBe('Print(v ...interface{})')
